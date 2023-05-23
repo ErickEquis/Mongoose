873988d0a7be5dbfbdf33660900b1755b3c5fcbe
ErickEquis <ericksantillan95.01@gmail.com>
2023-05-23 11:29:06 -0600

    Se actualiza proyecto mongoose. Se completa CRUD MongoDB con mongoose.

--- mongoose/src/pages/api/tasks/[id].ts
+++ mongoose/src/pages/api/tasks/[id].ts
@@ -1,3 +1,5 @@
+// Muestra la tarea en una pagina con ruta de [id]
+
 import { NextApiRequest, NextApiResponse } from "next"
 import Task from "../../../../models";
 
@@ -6,16 +8,43 @@ type Data = {
 
 export default async (req: NextApiRequest, res: NextApiResponse<Data>) => {
  
+    // Destructuracion de metodos 'req'
     const {method, body, query: { id }} = req;
+    
+    // Manejo de peticiones
     switch (method) {
+        
+        // Obtener tareas
         case 'GET':
             try {
+                // Busqueda por 'id'
                 const task = await Task.findById(id)
                 if (!task) return res.status(400).json({ msg: "Tarea no encontrada" })
-                return res.status(200).json(task)    
+                return res.status(200).json(task)
             } catch (error) {
                 return res.status(500).json({ msg: error })
             }
+        
+        // Actualizar tareas
+        case 'PUT':
+            try {
+                const updatedTask = await Task.findByIdAndUpdate(id , body, {new: true})
+                if (!updatedTask) return res.status(400).json({ msg: "Tarea no encontrada"})
+                return res.status(200).json(updatedTask)
+            } catch (error) {   
+                return res.status(500).json({ msg: error })
+            }
+
+        // Eliminar tareas
+        case 'DELETE':
+            try {
+                const deletedTask = await Task.findByIdAndDelete(id)
+                if (!deletedTask) return res.status(404).json({ msg: "Tarea no encontrada" })
+                return res.status(204).json(deletedTask)
+            } catch (error) {
+                return res.status(204).json({ msg: error })
+            }
+
         default:
             return res.status(400).json({msg: "Este metodo no es soportado"})
     }
--- mongoose/src/pages/index.tsx
+++ mongoose/src/pages/index.tsx
@@ -3,10 +3,28 @@ import { Inter } from 'next/font/google'
 
 const inter = Inter({ subsets: ['latin'] })
 
+// ------Frontend------
 export default function Home() {
   return (
     <>
-        <h1>Hola Mundo</h1>
+      <h1>Hola Mundo</h1>
     </>
   )
+}
+
+// Obteniendo tareas de BD
+// -----Backend-----
+export async function getServerSideProps() {
+
+  const res = await fetch('http://localhost:3000/api/tasks')
+  const tasks = await res.json()
+
+  console.log(tasks)
+
+  return {
+    props: {
+      tasks
+    }
+  }
+
 }
